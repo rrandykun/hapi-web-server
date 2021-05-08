169c1ec9b0d32aebb92e19121f887e06aa30086d
Randy AP <rrandy.kun@gmail.com>
2021-05-08 12:19:53 +0700

    feat: menambahkan path parameter

--- routes.js
+++ routes.js
@@ -1,39 +1,46 @@
-const routes = [
-  {
-    method: "GET",
-    path: "/",
-    handler: (request, h) => {
-      return "Homepage";
+const routes = [{
+        method: "GET",
+        path: "/",
+        handler: (request, h) => {
+            return "Homepage";
+        },
     },
-  },
-  {
-    method: "*",
-    path: "/",
-    handler: (request, h) => {
-      return "Halaman tidak dapat diakses dengan method tersebut";
+    {
+        method: "*",
+        path: "/",
+        handler: (request, h) => {
+            return "Halaman tidak dapat diakses dengan method tersebut";
+        },
     },
-  },
-  {
-    method: "GET",
-    path: "/about",
-    handler: (request, h) => {
-      return "About page";
+    {
+        method: "GET",
+        path: "/about",
+        handler: (request, h) => {
+            return "About page";
+        },
     },
-  },
-  {
-    method: "*",
-    path: "/about",
-    handler: (request, h) => {
-      return "Halaman tidak dapat diakses dengan method";
+    {
+        method: "*",
+        path: "/about",
+        handler: (request, h) => {
+            return "Halaman tidak dapat diakses dengan method";
+        },
     },
-  },
-  {
-    method: "*",
-    path: "/{any*}",
-    handler: (request, h) => {
-      return "Halaman tidak ditemukan";
+    {
+        method: "GET",
+        path: "/hello/{name?}",
+        handler: (request, h) => {
+            const { name } = request.params;
+            return `Hello, ${name}!`;
+        },
     },
-  },
+    {
+        method: "*",
+        path: "/{any*}",
+        handler: (request, h) => {
+            return "Halaman tidak ditemukan";
+        },
+    },
 ];
 
 module.exports = routes;
